69d726159219c3fc0d4a40c30d3b45ae822a3d2d
Loukas <loukasbass@gmail.com>
2024-11-20 16:14:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const counter3 = document.querySelector(".counter-3");
+
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 10; j++) {
+      const div = document.createElement("div");
+      div.className = "num";
+      div.textContent = j;
+      counter3.appendChild(div);
+    }
+  }
+
+  const finalDiv = document.createElement("div");
+  finalDiv.className = "num";
+  finalDiv.textContent = "0";
+  counter3.appendChild(finalDiv);
+
+  function animate(counter, dureation, delay = 0) {
+    const numHeight = counter.querySelector(".num");
+    clientHeight;
+    const totalDistance = counter.querySelectorAll(".num").length - 1;
+
+    gsap.to(counter, {
+      y: -totalDistance,
+      dureation: dureation,
+      delay: delay,
+      ease: "power2.inOut",
+    });
+
+    animate(counter3, 5);
+    animate(document.querySelector(".counter-2"), 6);
+    animate(document.querySelector(".counter-1"), 2, 4);
+  }
+});
